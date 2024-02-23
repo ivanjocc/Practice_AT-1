075b8f8d7bd43d41766e03c4d862986f59a176f8
ivan <ivanjose_04@hotmail.com>
2024-02-23 16:56:31 -0500

    first commit

---
+++ webPizza.aspx
@@ -0,0 +1,132 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="webPizza.aspx.cs" Inherits="prjWebCsControlleASP.webPizza" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title></title>
+    <style type="text/css">
+        body {
+            background-color: bisque;
+            color: brown;
+            font-weight: bold;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        marquee {
+            background-color: red;
+            color: white;
+            font-weight: bold;
+        }
+
+        .boite {
+            font-weight: bold;
+            width: 200px;
+            border-radius: 5px;
+            color: navy;
+        }
+
+        #grandTableau {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>TECCART - PIZZA - HURTS</h1>
+            <hr />
+            <marquee>La mejor pizza hecha por el tigro</marquee>
+            <hr />
+            <table id="grandTableau">
+                <tr>
+                    <td>
+                        <asp:Panel ID="panPizza" runat="server" BackColor="#ffc981" ForeColor="Red" Font-Bold="True" Width="450px" GroupingText="Information Client et Pizza">
+                <table>
+                    <tr>
+                        <td>Nom de client : </td>
+                        <td>
+                            <asp:TextBox ID="txtNom" runat="server" CssClass="boite"></asp:TextBox>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>Telephone du client : </td>
+                        <td>
+                            <asp:TextBox ID="txtTelephone" runat="server" CssClass="boite"></asp:TextBox>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Pour livraison ? : </td>
+                        <td>
+                            <asp:CheckBox ID="chkLivraison" runat="server" Font-Bold="True" Text="Oui" AutoPostBack="True" OnCheckedChanged="chkLivraison_CheckedChanged" />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="vertical-align: top;">
+                            <asp:Label ID="lblAdresse" runat="server" Text="Adress Livraison"></asp:Label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txtAdresse" runat="server" CssClass="boite" Height="50px"></asp:TextBox>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Choisir votre pizza : </td>
+                        <td>
+                            <asp:DropDownList ID="cboPizzas" runat="server" CssClass="boite" OnSelectedIndexChanged="cboPizzas_SelectedIndexChanged" AutoPostBack="true">
+                                <%--<asp:ListItem>Faites votre choix</asp:ListItem>
+                                <asp:ListItem>L&#39;italienne</asp:ListItem>
+                                <asp:ListItem>La vegetarienne</asp:ListItem>
+                                <asp:ListItem>La halal</asp:ListItem>
+                                <asp:ListItem>La mexicanaine</asp:ListItem>--%>
+                            </asp:DropDownList>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="vertical-align: top;">Choisir la taille : </td>
+                        <td>
+                            <asp:ListBox ID="lstTailles" runat="server" CssClass="boite" Height="100%" Rows="3" AutoPostBack="True" OnSelectedIndexChanged="lstTailles_SelectedIndexChanged">
+
+                            </asp:ListBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top;">Choisir garnitures : </td>
+                        <td>
+                            <asp:CheckBoxList ID="lstChkGarnitures" runat="server" CssClass="boite" OnSelectedIndexChanged="lstChkGarnitures_SelectedIndexChanged" AutoPostBack="true">
+
+                            </asp:CheckBoxList>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top;">Choisir Croute : </td>
+                        <td>
+                            <asp:RadioButtonList ID="lstRbtnCrust" runat="server" OnSelectedIndexChanged="lstRbtnCrust_SelectedIndexChanged" AutoPostBack="true">
+
+
+                            </asp:RadioButtonList>
+                        </td>
+                    </tr>
+                </table>
+            </asp:Panel>
+                    </td>
+                    <td style="vertical-align: top">
+                        <asp:Panel ID="PanPrix" runat="server" BackColor="#ffc981" ForeColor="Red" Font-Bold="True" Width="450px" GroupingText="Information Client et Pizza">
+                            <asp:Label ID="lblPrix" runat="server" Text="" ForeColor="Brown" Font-Bold="true"></asp:Label>
+                        </asp:Panel>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ webPizza.aspx.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace prjWebCsControlleASP
+{
+    public partial class webPizza : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                lblAdresse.Visible = false;
+                txtAdresse.Visible = false;
+                PanPrix.Visible = false;
+                RemplirListePizzas();
+                RemplirListeTaille();
+                RemplirListeGarnitures();
+                RemplirListeCroute();
+            }
+        }
+
+        private void RemplirListeCroute()
+        {
+            ListItem elm = new ListItem();
+            elm.Text = "normal (0 $)";
+            elm.Value = "0";
+            elm.Selected = true;
+            lstRbtnCrust.Items.Add(elm);
+
+            lstRbtnCrust.Items.Add(new ListItem("mince (0 $)", "0.0"));
+
+            lstRbtnCrust.Items.Add(new ListItem("epaisse (1 $)", "1"));
+
+            lstRbtnCrust.Items.Add(new ListItem("fournee fromage (2.5 $)", "2.5"));
+        }
+
+        private void RemplirListeGarnitures()
+        {
+            ListItem elm = new ListItem();
+            elm.Text = "bacon et jambon (2.5 $)";
+            elm.Value = "2.5";
+            lstChkGarnitures.Items.Add(elm);
+
+            lstChkGarnitures.Items.Add(new ListItem("champignon (1.5 $)", "1.5"));
+
+            lstChkGarnitures.Items.Add(new ListItem("extra fromage (1 $)", "1"));
+
+            lstChkGarnitures.Items.Add(new ListItem("pepperoni halal (2 $)", "2"));
+            
+        }
+
+        protected void CalculerPrix()
+        {
+            PanPrix.Visible = true;
+
+            decimal prixbase = 0;
+            decimal livraison = 0;
+            decimal soustot, total;
+            decimal tax = 0.15m;
+            decimal totalSansTax;
+            decimal prixGarnitures = 0;
+            decimal prixCrust = 0;
+            string info;
+
+            prixbase = Convert.ToDecimal(cboPizzas.SelectedItem.Value);
+
+            prixbase = prixbase * Convert.ToDecimal(lstTailles.SelectedItem.Value);
+
+            foreach (ListItem garniture in lstChkGarnitures.Items)
+            {
+                if (garniture.Selected)
+                {
+                    prixGarnitures += Convert.ToDecimal(garniture.Value);
+                }
+            }
+
+            if (chkLivraison.Checked)
+            {
+                livraison = 5;
+            }
+
+            prixCrust += Convert.ToDecimal(lstRbtnCrust.SelectedItem.Value);
+
+            soustot = prixbase + livraison + prixGarnitures + prixCrust;
+
+            totalSansTax = soustot * tax;
+
+            total = soustot + totalSansTax;
+
+            info = "le prix de base est: " + prixbase + " $ <br>";
+            info += "frais de livraison " + livraison + " $ <br>";
+            info += "prix des garnitures " + prixGarnitures + " $ <br>";
+            info += "prix de croute " + prixCrust+ " $ <br>";
+            info += "---------------------------------------------------- <br>";
+            info += "sous-total " + soustot+ " $ <br>";
+            info += "tax (15%) " + Math.Round(totalSansTax, 2) + " $ <br>";
+            info += "---------------------------------------------------- <br>";
+            info += "total " + Math.Round(total, 2) + " $ <br>";
+
+
+            lblPrix.Text = info;
+        }
+
+        private void RemplirListeTaille()
+        {
+            ListItem elm = new ListItem();
+            elm.Text = "petite";
+            elm.Value = "1";
+            elm.Selected = true;
+            lstTailles.Items.Add(elm);
+
+            lstTailles.Items.Add(new ListItem("moyenne", "1.5"));
+
+            lstTailles.Items.Add(new ListItem("grande", "2"));
+        }
+
+        private void RemplirListePizzas()
+        {
+            ListItem elm = new ListItem();
+            elm.Text = "Faites votre choix";
+            elm.Value = "0";
+            cboPizzas.Items.Add(elm);
+
+            elm = new ListItem();
+            elm.Text = "L'italienne";
+            elm.Value = "10";
+            cboPizzas.Items.Add(elm);
+
+            elm = new ListItem("la atunia", "9");
+            cboPizzas.Items.Add(elm);
+
+            cboPizzas.Items.Add(new ListItem("la choriperro", "11"));
+
+            cboPizzas.Items.Add(new ListItem("la vegetarienne", "14"));
+
+            cboPizzas.Items.Add(new ListItem("la halal", "3"));
+        }
+
+        protected void chkLivraison_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkLivraison.Checked == true)
+            {
+                lblAdresse.Visible = true;
+                txtAdresse.Visible = true;
+                txtAdresse.Focus();
+            }
+            else
+            {
+                lblAdresse.Visible = false;
+                txtAdresse.Visible = false;
+            }
+
+            if (cboPizzas.SelectedIndex != 0)
+            {
+                CalculerPrix();
+            } else
+            {
+                PanPrix.Visible = false;
+            }
+        }
+
+        protected void cboPizzas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboPizzas.SelectedIndex != 0)
+            {
+                CalculerPrix();
+            }
+        }
+
+        protected void lstTailles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboPizzas.SelectedIndex != 0)
+            {
+                CalculerPrix();
+            } else
+            {
+                PanPrix.Visible = false;
+            }
+        }
+
+        protected void lstChkGarnitures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboPizzas.SelectedIndex != 0)
+            {
+                CalculerPrix();
+            }
+            else
+            {
+                PanPrix.Visible = false;
+            }
+        }
+
+        protected void lstRbtnCrust_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboPizzas.SelectedIndex != 0)
+            {
+                CalculerPrix();
+            }
+            else
+            {
+                PanPrix.Visible = false;
+            }
+        }
+    }
+}
